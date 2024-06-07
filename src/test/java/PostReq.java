9d76d770613144913239094173bb6af53c94fb15
Sundarraj R <ragusundar274@gmail.com>
2024-06-07 21:41:47 +0530

    Different ways of passing request body

--- src/test/java/PostReq.java
+++ src/test/java/PostReq.java
@@ -6,6 +6,9 @@ import io.restassured.http.ContentType;
 import io.restassured.mapper.ObjectMapperDeserializationContext;
 import io.restassured.mapper.ObjectMapperSerializationContext;
 import io.restassured.response.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -18,8 +21,9 @@ import static io.restassured.RestAssured.*;
 
 public class PostReq {
     @Test
-    // 1. Approach Passing json body as String --Not Recommended
     public void postReq1(){
+        // 1. Approach Passing json body as String --Not Recommended
+
         String reqBody="{\n" +
                 "    \"id\": \"e816\",\n" +
                 "    \"first_name\": \"sundar\",\n" +
@@ -47,10 +51,12 @@ public class PostReq {
 
 
     @Test
-    //Approach-2 : Passing Data from an external file
-    //1.Can't get the request content from the file and print it on the console.
-    //2.Use only for static data.
     public void postReq2(){
+
+        //Approach-2 : Passing Data from an external file
+        //1.Can't get the request content from the file and print it on the console.
+        //2.Use only for static data.
+
         File f=new File("C:\\Users\\ragus\\IdeaProjects\\RestAssured_Practice\\src\\main\\resources\\sampleReqBody.json");
 
         Response response=given()
@@ -123,11 +129,9 @@ public class PostReq {
          * De-Serialization - json -> byte streams to coded language back
          * Add Jackson databind library or gson
          * */
-        String reqBody=new ObjectMapper().writeValueAsString(lm);
-
         Response response=given()
                 .header("Content-Type",ContentType.JSON)
-                .body(reqBody)
+                .body(lm)
                 .log()
                 .all()
                 .post("http://localhost:3000/students");
@@ -138,28 +142,31 @@ public class PostReq {
     @Test
     public void postReq5(){
         /*
-        * Using Map and List from Java
-        * {} -> JsonObject
-        * []-> JsonArray
-        * Add JSON
-        *
-        {
+        * {
         "id": "un",
         "first_name": "fname",
         "last_name": "lname",
         "email": "emailAdd",
         "isPassed": true,
         *"marks":[95,87,98,99,100]
-         }
-        * */
+         }*/
+        JSONObject jo = new JSONObject();
+        jo.put("id",new Faker().number().numberBetween(100,1000));
+        jo.put("first_name",new Faker().name().firstName());
+        jo.put("last_name",new Faker().name().lastName());
+        jo.put("email",new Faker().internet().emailAddress());
+        jo.put("isPassed",new Faker().bool().bool());
+        JSONArray ja = new JSONArray();
+        ja.put(new Faker().number().numberBetween(50,100));
+        ja.put(new Faker().number().numberBetween(50,100));
+        ja.put(new Faker().number().numberBetween(50,100));
+        ja.put(new Faker().number().numberBetween(50,100));
+        ja.put(new Faker().number().numberBetween(50,100));
+        jo.put("marks",ja);
 
-        /*Serialization -Converts Code language into bye streams-->json
-         * De-Serialization - json -> byte streams to coded language back
-         * Add Jackson databind library or gson
-         * */
         Response response=given()
                 .header("Content-Type",ContentType.JSON)
-                .body(reqBody)
+                .body(jo.toString()) // use toString() or toMap()
                 .log()
                 .all()
                 .post("http://localhost:3000/students");
